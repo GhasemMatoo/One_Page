47ec2acc0909b0dfab03dfa692a2e18bb45965a9
GhasemMatoo <82996655+GhasemMatoo@users.noreply.github.com>
2021-09-12 05:03:04 +0430

    update seting

--- OnePage/settings.py
+++ OnePage/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
